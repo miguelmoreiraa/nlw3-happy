ef0cae8ad36cc4ea34d241dcdd7e74cfac4590b4
miguelmoreirabandtec <miguel.moreira@bandtec.com.br>
2020-11-03 16:11:17 -0300

    :rocket: Implantando telas novas

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import CreateOrphanage from './pages/CreateOrphanage';
+import Orphanage from './pages/Orphanage';
+
 
 
 
@@ -12,6 +15,8 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={OrphanagesMap} />
+                <Route path="/orphanages/create" component={CreateOrphanage} />
+                <Route path="/orphanages/:id" component={Orphanage} />
             </Switch>
 
         </BrowserRouter>
